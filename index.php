94e019a51f6901c093181a9f303e27cd2f4e113f
FireIceMage <rampageeebrawlstars7910@gmail.com>
2022-12-25 01:11:08 +0300

    Pictures to posts add

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     date_default_timezone_set('Europe/Moscow');
     include 'connection.php';
     include 'comment.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -18,18 +17,21 @@
 <h1>Основы программирования</h1>
 <?php
 
-echo "<form method='POST' action='".setComments($conn)."'>
+
+echo "<form method='POST' action='".setComments($conn)."' enctype = 'multipart/form-data'>
     <input type='hidden' name='uid' value='Аноним'>
     <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
     <input type='hidden' name='likes' value='0'>
+    <input type='hidden' name='dislikes' value='0'>
     <textarea name='message' required></textarea><br>
     <button type='submit' class='commentSubmit' name='commentSubmit'>Оставить сообщение</button>
-    <hr class='hr'>
-</form>";
-
+    <br><input type='file' name='img' style='margin-left: 25px'>
+    </form>";
 ?>
 
+<hr class='hr'>
 <div class="flex-container">
+
 <?php
 
 echo "<form method='POST' action='".getComments($conn)."'>
